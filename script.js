80035ee4de308eeeab22444997d379093460da05
ayu-g22 <95233360+ayu-g22@users.noreply.github.com>
2022-04-13 14:25:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+{function myFunction(){
+    var d1=document.getElementById("detail1");
+        if (d1.style.display !== "none") {
+          d1.style.display = "none";
+        } else {
+          d1.style.display = "block";
+        }
+        d1.classList.add("animate");
+}}
+{function myFun(){
+    var d2=document.getElementById("detail2");
+        if (d2.style.display !== "none") {
+          d2.style.display = "none";
+        } else {
+          d2.style.display = "block";
+        }
+        d2.classList.add("animate");
+}}
